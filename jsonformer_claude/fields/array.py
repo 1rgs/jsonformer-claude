a38e356101ff7d77ca1774db228eb466c7d86cf4
Rodda John <johnrod.john@gmail.com>
2023-05-21 10:35:30 -0400

    ignoring arrays for now

--- jsonformer_claude/fields/array.py
+++
@@ -1,8 +0,0 @@
-from jsonformer_claude.fields.base import BaseField
-
-
-class ArrayField(BaseField):
-    end_tokens = ["]"]
-
-    def insert_generation_marker(self):
-        self.obj[self.key] = [self.generation_marker]
